9867e993aad9ac79323b6643497ef99de1bb1b2a
Norbert Auer <norbert.auer@boku.ac.at>
2016-07-27 12:12:22 +0200

    Version: 1.1 Add test folder with example fasta files
    Fix function overloading issue (not possible in Python)
    
    load_fasta(self, *, file_path) -> load_fasta_from_file(self, *file_path)
    
    Allows now reading from multiple files

--- BioPylib.py
+++ BioPylib.py
@@ -8,7 +8,7 @@ import pprint
 
 #from Bio.Blast.Applications import NcbiblastnCommandline
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 class Alphabeth():
     DNA = {'name' : "DNA", 'alphabeth' : b'ACGT', 'rev_translation' : b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNTNGNNNCNNNNNNNNNNNNANNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff', 'translation' : b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNANCNNNGNNNNNNNNNNNNTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff'}
@@ -214,12 +214,13 @@ class MultiFasta():
     def remove_all(self):
         self.fastas = []
 
-    def load_fasta(self, *, file_path):
-        with open(file_path, "rb") as fastafile:
-            self.load_fasta(file_obj=fastafile)
+    def load_fasta_from_file(self, *file_path):
+        for path in file_path:
+            with open(path, "rb") as fasta_file_handler:
+                self.load_fasta(file_obj=fasta_file_handler)
 
-    def load_fasta(self, *, file_obj):
-        [self.fastas.append(fasta) for fasta in MultiFasta.read_fasta(file_obj=file_obj)]
+    def load_fasta(self, file_obj):
+        [self.fastas.append(fasta) for fasta in MultiFasta.read_fasta(file_obj)]
 
     def add_fasta(self, fasta):
         if isinstance(fasta, Fasta):
@@ -232,12 +233,12 @@ class MultiFasta():
                 handler.write(fasta.get_sequence() + b"\n")
 
     @staticmethod
-    def read_fasta(*, file_path, header_pattern=b"^>"):
-        with open(file_path, "rb") as fastafile:
-            Fasta.read_fasta(file_obj=fastafile, header_pattern=header_pattern)
+    def read_fasta_from_file(*, file_path, header_pattern=b"^>"):
+        with open(file_path, "rb") as fasta_file_handler:
+            MultiFasta.read_fasta(fasta_file_handler, header_pattern=header_pattern)
 
     @staticmethod
-    def read_fasta(*, file_obj, header_pattern=b"^>"):
+    def read_fasta(file_obj, header_pattern=b"^>"):
         try:
             header_pattern = header_pattern.encode()
         except AttributeError:
@@ -443,3 +444,9 @@ if __name__ == "__main__":
     fasta = Fasta(">test", "GGGATTACCAGTGGTCACACTTGGAAACGTTGTTGCCGTGCTACAGTCACGTCAT")
     print(fasta.get_GC_content())
     print(fasta.get_reverse_transcript_sequence())
+
+    mf = MultiFasta()
+    mf.load_fasta_from_file("test/test.fa", "test/test2.fa")
+
+    for fa in mf:
+        print(fa)
